ecca4f00451fb5e2628ad0526673c6ae3ccaf587
penpizza <penpicha_thongkham@gmail.com>
2022-12-09 16:29:30 +0700

    lab6_1,2 complete

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num=1;
+    int odd=0;
+    int even=0;
+    while(num !=0){
+        cout << "Enter an integer: ";
+        cin>>num;
+        if (num%2 ==0 && num !=0){
+            even++;
+        }  
+        if (num%2 !=0) {
+            odd++;
+        }
+        }
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double deg2rad(double a){
+    return a*(M_PI/180.00);
+}
+double rad2deg(double a){
+    return a*(180.00/M_PI);
+}
+double findXComponent(double la,double lb,double a,double b){
+    double x,y,z;
+    x=la*cos(a);
+    y=lb*cos(b);
+    z=x+y;
+    return z; 
+}
+double findYComponent(double la,double lb,double a,double b){
+    double x,y,z;
+    x=la*sin(a);
+    y=lb*sin(b);
+    z=x+y;
+    return z; 
+}
+double pythagoras(double x,double y){
+    double z;
+    return z=sqrt(pow(x,2)+pow(y,2));
+}
+void showResult(double a,double b){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+    cout << "Length of the resultant vector = "<< a <<endl;
+    cout << "Direction of the resultant vector (deg) = " << b<<endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
@@ -12,7 +42,7 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
-    
+
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
     xcomp = findXComponent(l1,l2,a1,a2);
